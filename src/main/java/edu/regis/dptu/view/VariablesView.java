3d071b07aaa2c31a5ca0f1900b4c038b2086b497
bplewinski <carrbrian13@gmail.com>
2024-05-04 15:16:14 +0900

    Construct Variable text fields for algorithm

--- src/main/java/edu/regis/dptu/view/VariablesView.java
+++ src/main/java/edu/regis/dptu/view/VariablesView.java
@@ -1,54 +1,100 @@
 package edu.regis.dptu.view;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 /**
+ * Represents a JPanel for displaying variables related to a specific algorithm or computation.
+ * Provides labeled text fields for each variable.
  * 
  * @author bplewinski
  */
 public class VariablesView extends JPanel {
 
-   // Labels for displaying variable names and their values
-   private JLabel labelSequenceId;
-   private JLabel labelCurrentStepIndex;
+   // Labels and text fields for displaying variables
+   private final JLabel labelI = new JLabel("i");
+   private final JTextField valueI = new JTextField(3);
 
-   // Text fields to display the values of the variables
-   private JTextField textFieldSequenceId;
-   private JTextField textFieldCurrentStepIndex;
+   private final JLabel labelJ = new JLabel("j");
+   private final JTextField valueJ = new JTextField(3);
 
+   private final JLabel labelXI = new JLabel("x[i]");
+   private final JTextField valueXI = new JTextField(3);
+
+   private final JLabel labelYJ = new JLabel("y[j]");
+   private final JTextField valueYJ = new JTextField(3);
+
+   private final JLabel labelLPrev1 = new JLabel("L[i-1, j]");
+   private final JTextField valueLPrev1 = new JTextField(3);
+
+   private final JLabel labelLPrev2 = new JLabel("L[i, j-1]");
+   private final JTextField valueLPrev2 = new JTextField(3);
+
+   private final JLabel labelLCurrent = new JLabel("L[i, j]");
+   private final JTextField valueLCurrent = new JTextField(3);
+
+   /**
+    * Constructs the VariablesView JPanel and initializes its components.
+    */
    public VariablesView() {
-      
-      initComponents();
+      setLayout(new GridBagLayout());
+      GridBagConstraints gbc = new GridBagConstraints();
+      gbc.insets = new Insets(2, 2, 2, 2);
+      gbc.gridx = 0;
+      gbc.gridy = 0;
+
+      addComponent(labelI, valueI, gbc, 0);
+      addComponent(labelJ, valueJ, gbc, 2);
+      addComponent(labelXI, valueXI, gbc, 4);
+      addComponent(labelYJ, valueYJ, gbc, 6);
+      addComponent(labelLPrev1, valueLPrev1, gbc, 8);
+      addComponent(labelLPrev2, valueLPrev2, gbc, 10);
+      addComponent(labelLCurrent, valueLCurrent, gbc, 12);
+
+      valueI.setEditable(false);
+      valueJ.setEditable(false);
+      valueXI.setEditable(false);
+      valueYJ.setEditable(false);
+      valueLPrev1.setEditable(false);
+      valueLPrev2.setEditable(false);
+      valueLCurrent.setEditable(false);
    }
 
-   private void initComponents() {
-      setLayout(new GridLayout(2, 2)); // 2 rows, 2 columns
-
-      // Initialize labels for variable names
-      labelSequenceId = new JLabel("Sequence ID:");
-      labelCurrentStepIndex = new JLabel("Current Step Index:");
-
-      // Initialize text fields for variable values
-      textFieldSequenceId = new JTextField();
-      textFieldCurrentStepIndex = new JTextField();
-
-      // Make text fields uneditable
-      textFieldSequenceId.setEditable(false);
-      textFieldCurrentStepIndex.setEditable(false);
-
-      // Add labels and text fields to the panel
-      add(labelSequenceId);
-      add(textFieldSequenceId);
-      add(labelCurrentStepIndex);
-      add(textFieldCurrentStepIndex);
+   /**
+    * Adds a label and a text field to the panel with the given constraints.
+    * 
+    * @param label The label to be added.
+    * @param field The text field to be added.
+    * @param gbc The GridBagConstraints specifying the position of the components.
+    * @param gridx The horizontal position for the label.
+    */
+   private void addComponent(JLabel label, JTextField field, GridBagConstraints gbc, int gridx) {
+      gbc.gridx = gridx;
+      add(label, gbc);
+      gbc.gridx = gridx + 1;
+      add(field, gbc);
    }
-
-   // Method to update the values of variables displayed in the view
-   public void updateVariables(int sequenceId, int currentStepIndex) {
-      // Update the text fields with the provided variables
-      textFieldSequenceId.setText(String.valueOf(sequenceId));
-      textFieldCurrentStepIndex.setText(String.valueOf(currentStepIndex));
+   /**
+    * Updates the displayed values of the variables.
+    * 
+    * @param i The value of variable i.
+    * @param j The value of variable j.
+    * @param xi The value of variable x[i].
+    * @param yj The value of variable y[j].
+    * @param Lprev1 The value of variable L[i-1, j].
+    * @param Lprev2 The value of variable L[i, j-1].
+    * @param Lcurrent The value of variable L[i, j].
+    */
+   public void updateValues(int i, int j, char xi, char yj, int Lprev1, int Lprev2, int Lcurrent) {
+      // Print out the variable values for debugging
+      valueI.setText(String.valueOf(i));
+      valueJ.setText(String.valueOf(j));
+      valueXI.setText(String.valueOf(xi));
+      valueYJ.setText(String.valueOf(yj));
+      valueLPrev1.setText(String.valueOf(Lprev1));
+      valueLPrev2.setText(String.valueOf(Lprev2));
+      valueLCurrent.setText(String.valueOf(Lcurrent));
    }
 }
-
