dde917b8f909a04da212575cd32a08d374f87a7d
Sofia Reyes <sofiareyes119@gmail.com>
2025-02-06 15:39:18 -0700

    Removed unecessary imports Student Model

--- src/main/java/edu/regis/dptu/model/aol/StudentModel.java
+++ src/main/java/edu/regis/dptu/model/aol/StudentModel.java
@@ -12,57 +12,43 @@
  */
 package edu.regis.dptu.model.aol;
 
-import edu.regis.dptu.model.*;
-import edu.regis.dptu.err.NonRecoverableException;
-import static edu.regis.dptu.model.Model.DEFAULT_ID;
 import edu.regis.dptu.model.ScaffoldLevel;
-import edu.regis.dptu.util.XmlMgr;
-import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import javax.xml.parsers.DocumentBuilder; 
-import javax.xml.parsers.DocumentBuilderFactory; 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer; 
-import javax.xml.transform.TransformerFactory; 
-import javax.xml.transform.dom.DOMSource; 
-import javax.xml.transform.stream.StreamResult; 
-import org.w3c.dom.Document; 
-import org.w3c.dom.Element; 
-import org.w3c.dom.NodeList;
 
 /**
- * Captures the current assessment for each learning outcome in a course, as 
+ * Captures the current assessment for each learning outcome in a course, as
  * well as, all tutoring sessions in which the student participated.
- * 
+ *
  * @author rickb
  */
 public class StudentModel {
+
     /**
      * Convenience reference to the user id (email) of the student associated
      * with this student model.
      */
     private String userId;
-    
+
     /**
      * The assessments of outcomes for the student associated with this model.
-     * The key is the id of the knowledge component in the associated assessment.
+     * The key is the id of the knowledge component in the associated
+     * assessment.
      */
     private HashMap<Integer, Assessment> assessments;
-    
+
     /**
      * The current scaffolding being used to support the student.
      */
     private ScaffoldLevel scaffoldLevel = ScaffoldLevel.EXTREME;
-    
+
     /**
      * Instantiate this student model with default information.
-     * 
+     *
      * @param userId the user id of the student whose model is being created.
      */
     public StudentModel(String userId) {
         this.userId = userId;
-        
+
         assessments = new HashMap<>();
     }
 
@@ -73,36 +59,36 @@ public class StudentModel {
     public void setUserId(String userId) {
         this.userId = userId;
     }
-    
+
     public void addAssessment(int knowledgeComponentId, Assessment assessment) {
         assessments.put(knowledgeComponentId, assessment);
     }
 
     /**
      * Return whether this student has an assessment for the given outcome.
-     * 
-     * @param outcome
+     *
+     * @param knowledgeComponentId
      * @return true if the student has an assessment for the given outcome.
      */
     public boolean containsAssessment(int knowledgeComponentId) {
         return assessments.containsKey(knowledgeComponentId);
     }
-    
+
     /**
      * Return the student assessment, if any, for the given outcome.
-     * 
-     * @param outcome the Outcome being accessed
-     * 
+     *
+     * @param knowledgeComponentId
+     *
      * @return an Assessment of the student.
      */
     public Assessment findAssessment(int knowledgeComponentId) {
         return assessments.get(knowledgeComponentId);
     }
-    
+
     /**
      * Return the current scaffolding level being used to support the student.
-     * 
-     * @return 
+     *
+     * @return
      */
     public ScaffoldLevel getScaffoldLevel() {
         return scaffoldLevel;
