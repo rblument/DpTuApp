951291d0244230746305aa103c252f96b5d13823
Daniela0925 <dflorespereida@regis.edu>
2025-04-22 21:30:48 -0600

    In progress Integrate LCS Problem into VariablesView

--- src/main/java/edu/regis/dptu/view/VariablesView.java
+++ src/main/java/edu/regis/dptu/view/VariablesView.java
@@ -12,30 +12,47 @@
  */
 package edu.regis.dptu.view;
 
+import edu.regis.dptu.model.LCSProblem;
 import edu.regis.dptu.model.Problem;
+import edu.regis.dptu.model.ProblemListener;
 import java.awt.GridBagConstraints;
+import java.util.ArrayList;
 import javax.swing.JLabel;
-import javax.swing.GPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
  * @author danielaflores
  */
-public class VariablesView extends GPanel {
+public class VariablesView extends GPanel implements ProblemListener{
     private Problem model;
-    private JLabel rName, rValue, cName, cValue, iName, iValue, jName, jValue, lName, lValue;
+    private JLabel variableNamesLabel;
+    //private ArrayList<JLabel> variableNamesLabel;
+    private JLabel variableValuesLabel;
+    //private ArrayList<JLabel> variableValuesLabel;
 
     public VariablesView() {
-        initializeComponents();
-        layoutComponents();
-    }
-    
-    public void setModel(Problem model){
-        this.model = model;
-        updateView();
+        String x = "skullandbones"; // n == 13
+        String y = "lullabybabies";
+        
+        model = new LCSProblem(x,y);
+        
+        model.step();
+        //initializeComponents();
+        //layoutComponents();
     }
      
-    private void initializeComponents(){
+    public void setModel(Problem model){
+        this.model = model;
+        model.addProblemListener(this);
+        updateView();
+    }
+    
+    public Problem getModel() {
+        return model;
+    }
+     
+    /*private void initializeComponents(){
          rName = new JLabel("r = ");
          rValue = new JLabel("13");
          cName = new JLabel("c = ");
@@ -89,8 +106,46 @@ public class VariablesView extends GPanel {
                 GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
                 5, 5, 5, 5);
     }
+    */
     
     private void updateView() {
+        removeAll();
+        ArrayList<String> pVariableNames = model.getVariableNames();
+        //System.out.println(pVariableNames);
+        //int pVariableValue = model.getVariableValue(pVariableNames.toString());
+        
+        GPanel childComponents = new GPanel();
+        
+        for(int i = 0; i< pVariableNames.size(); i++){
+            String localVariableName = pVariableNames.get(i);
+            
+            //variableNamesLabel = new ArrayList<JLabel> ();
+            //variableNamesLabel.add(new JLabel(localVariableName));
+            
+            variableNamesLabel = new JLabel(localVariableName);
+            //variableValuesLabel = new ArrayList<JLabel> ();
+            //variableValuesLabel.add(String.valueOf(model.getVariableValue(localVariableName)));
+            
+            variableValuesLabel = new JLabel(String.valueOf(model.getVariableValue(localVariableName)));
+            
+            childComponents.addc(variableNamesLabel,0,i,1,1,0.0,0.0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+                    5,5,5,5);
+            
+            childComponents.addc(variableValuesLabel,1,i,1,1,0.0,0.0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+                    5,5,5,5);
+        }
+        
+        JScrollPane scrollPane = new JScrollPane(childComponents);
+        childComponents.addc(scrollPane,0,0,1,1,0.0,0.0,
+                    GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+                    5,5,5,5);
+
+        revalidate();
+        repaint();
+        
+        /*
         if (model == null) {
             return;
         }
@@ -106,5 +161,10 @@ public class VariablesView extends GPanel {
             jValue.setText(String.valueOf(lcsProblem.getJ()));
         }
         */
+    }
+
+    @Override
+    public void problemUpdated(Problem problem) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
