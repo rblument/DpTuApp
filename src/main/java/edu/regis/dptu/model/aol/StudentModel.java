393386a10d60f7486cfaa477c1aee74f19f5bd9d
sknoll-eng <sknoll@regis.edu>
2025-09-26 05:08:10 -0400

    DPTU-21 remove unused code in StudentModel
    Removed a commented out line from StudentModel.java as part of unused code cleanup.

--- src/main/java/edu/regis/dptu/model/aol/StudentModel.java
+++ src/main/java/edu/regis/dptu/model/aol/StudentModel.java
@@ -48,7 +48,7 @@ public class StudentModel {
     public StudentModel(String userId) {
         this.userId = userId;
 
-        // assessments = new HashMap<>();
+        
         assessments = new HashMap<>();
     }
 
