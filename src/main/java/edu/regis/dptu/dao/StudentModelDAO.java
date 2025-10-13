8f1f896e8058e36c781896234ff46bdec8d15cfb
GitHub Action <action@github.com>
2025-10-13 03:33:51 +0000

    Auto-format code with Spotless

--- src/main/java/edu/regis/dptu/dao/StudentModelDAO.java
+++ src/main/java/edu/regis/dptu/dao/StudentModelDAO.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 import edu.regis.dptu.err.NonRecoverableException;
 import edu.regis.dptu.err.ObjNotFoundException;
+import edu.regis.dptu.model.Account;
 import edu.regis.dptu.model.Course;
 import edu.regis.dptu.model.KnowledgeComponent;
 import edu.regis.dptu.model.ScaffoldLevel;
 import edu.regis.dptu.model.Student;
-import edu.regis.dptu.model.Account;
 import edu.regis.dptu.model.StudentModelFieldKind;
 import edu.regis.dptu.model.TutoringSession;
 import edu.regis.dptu.model.aol.Assessment;
