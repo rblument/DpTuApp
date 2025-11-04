485adf9d7693c64021988cf8926fdd9b8394bd57
GitHub Action <action@github.com>
2025-11-04 04:11:46 +0000

    Auto-format code with Spotless

--- src/main/java/edu/regis/dptu/view/act/DpTuGuiAction.java
+++ src/main/java/edu/regis/dptu/view/act/DpTuGuiAction.java
@@ -12,12 +12,13 @@
  */
 package edu.regis.dptu.view.act;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.awt.Image;
+
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.regis.dptu.util.ImgFactory;
 
