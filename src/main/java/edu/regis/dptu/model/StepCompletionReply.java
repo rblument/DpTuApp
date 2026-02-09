57b364c7ede695c74295dafdcc17dff688df79a6
Harrison Sherwin <70494788+hasherwi@users.noreply.github.com>
2026-02-09 13:41:37 -0700

    Remove unused logger and add suppression
    Remove SLF4J Logger and related imports from StepCompletionReply and add @SuppressWarnings("Logging") to suppress unused-logger warnings. Minor cleanup to remove dead code.

--- src/main/java/edu/regis/dptu/model/StepCompletionReply.java
+++ src/main/java/edu/regis/dptu/model/StepCompletionReply.java
@@ -11,18 +11,13 @@
  *  or conditions of any kind, either expressed or implied.
  */
 package edu.regis.dptu.model;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Captures the tutor's reply to a previous step completed requests.
  *
  * @author rickb
  */
+@SuppressWarnings("Logging")
 public class StepCompletionReply {
-    private static final Logger log = LoggerFactory.getLogger(StepCompletionReply.class);
-
     /** Was the student's submitted answer correct. */
     private boolean isCorrect;
 
