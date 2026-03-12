25a2c0557fd30b63d6325bb7982228ac0dee75e1
GitHub Action <action@github.com>
2026-03-12 17:59:13 +0000

    Auto-format code with Spotless

--- src/main/java/edu/regis/dptu/view/StatsWindow.java
+++ src/main/java/edu/regis/dptu/view/StatsWindow.java
@@ -1,10 +1,11 @@
 package edu.regis.dptu.view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 
 public class StatsWindow extends JFrame {
     public StatsWindow(String title) {
@@ -13,9 +14,8 @@ public class StatsWindow extends JFrame {
         JLabel placeholder = new JLabel("Stats will appear here", SwingConstants.CENTER);
         add(placeholder, BorderLayout.CENTER);
         setSize(new Dimension(400, 300));
-        setLocationRelativeTo(null); //center on screen
+        setLocationRelativeTo(null); // center on screen
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
     }
 }
-
