b2e4d3824be07f9db33be4be34a8972006dc38c7
CoxL0312 <lindseycox0312@gmail.com>
2026-04-12 20:47:23 -0600

    Update SignInAction.java
    Adding a catch for if reply.getData() isn't valid JSON.

--- src/main/java/edu/regis/dptu/view/act/SignInAction.java
+++ src/main/java/edu/regis/dptu/view/act/SignInAction.java
@@ -82,6 +82,13 @@ public class SignInAction extends DpTuGuiAction {
                 try {
                     JsonObject sessionJson =
                             JsonParser.parseString(reply.getData()).getAsJsonObject();
+                    if (!sessionJson.has("id") || !sessionJson.has("securityToken")) {
+                        log.error("SIGN_IN reply missing required session fields: {}", reply.getData());
+                        SplashFrame.instance().showError(
+                            ResourceMgr.instance().string("dialog.title.error"),
+                            "Login succeeded but session data was incomplete");
+                        return;
+                    }
                     int sessionId = sessionJson.get("id").getAsInt();
                     String securityToken = sessionJson.get("securityToken").getAsString();
                     TutoringSession signedInSession = new TutoringSession(user.getUserId());
