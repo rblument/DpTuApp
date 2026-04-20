b88ad0f6668cf4fcf8468688082ac05005c1a8c9
Harrison Sherwin <harrison.sherwin@gmail.com>
2026-04-19 19:02:27 -0600

    Add mode handling and pending task/step management to SessionDAOTest

--- src/test/java/edu/regis/dptu/test/SessionDAOTest.java
+++ src/test/java/edu/regis/dptu/test/SessionDAOTest.java
@@ -35,10 +35,17 @@ import edu.regis.dptu.err.ObjNotFoundException;
 import edu.regis.dptu.model.Account;
 import edu.regis.dptu.model.CourseDigest;
 import edu.regis.dptu.model.LCSProblem;
+import edu.regis.dptu.model.Mode;
+import edu.regis.dptu.model.PendingStep;
+import edu.regis.dptu.model.PendingTask;
 import edu.regis.dptu.model.Problem;
+import edu.regis.dptu.model.Step;
+import edu.regis.dptu.model.StepSubType;
 import edu.regis.dptu.model.Student;
+import edu.regis.dptu.model.Task;
 import edu.regis.dptu.model.TutoringSession;
 import edu.regis.dptu.model.UnitDigest;
+import edu.regis.dptu.svc.CourseSvc;
 import edu.regis.dptu.svc.ProblemSvc;
 import edu.regis.dptu.svc.ServiceFactory;
 
@@ -73,18 +80,40 @@ public class SessionDAOTest {
     @Test
     public void testCreateSuccess() throws Exception {
         TutoringSession session = buildSession(100, "student@regis.edu");
+        session.setMode(Mode.DO_ONE);
+
+        PendingTask pendingTask = new PendingTask(new Task(13));
+        PendingStep pendingStep = new PendingStep(new Step(21, 0, StepSubType.INFO_MESSAGE));
+        pendingStep.setCurrentHintIndex(2);
+        pendingStep.setNotifyTutor(true);
+        pendingStep.setIsCompleted(true);
+        pendingTask.setCurrentStep(pendingStep);
+        session.addTask(pendingTask);
 
         PreparedStatement existsStmt = mock(PreparedStatement.class);
         ResultSet existsRs = mock(ResultSet.class);
         PreparedStatement insertStmt = mock(PreparedStatement.class);
+        PreparedStatement insertPendingStepStmt = mock(PreparedStatement.class);
+        PreparedStatement insertPendingTaskStmt = mock(PreparedStatement.class);
+        ResultSet keyRs = mock(ResultSet.class);
 
         when(mockConnection.prepareStatement(contains("SELECT SessionId"))).thenReturn(existsStmt);
         when(mockConnection.prepareStatement(
                         startsWith("INSERT INTO TutoringSession"), any(String[].class)))
                 .thenReturn(insertStmt);
+        when(mockConnection.prepareStatement(
+                startsWith("INSERT INTO PendingStep"), anyInt()))
+            .thenReturn(insertPendingStepStmt);
+        when(mockConnection.prepareStatement(startsWith("INSERT INTO PendingTask")))
+            .thenReturn(insertPendingTaskStmt);
         when(existsStmt.executeQuery()).thenReturn(existsRs);
         when(existsRs.next()).thenReturn(false);
         when(insertStmt.executeUpdate()).thenReturn(1);
+        when(insertPendingStepStmt.executeUpdate()).thenReturn(1);
+        when(insertPendingTaskStmt.executeUpdate()).thenReturn(1);
+        when(insertPendingStepStmt.getGeneratedKeys()).thenReturn(keyRs);
+        when(keyRs.next()).thenReturn(true);
+        when(keyRs.getInt(1)).thenReturn(44);
 
         assertDoesNotThrow(() -> dao.create(session));
 
@@ -95,6 +124,12 @@ public class SessionDAOTest {
         verify(insertStmt).setBoolean(5, session.isIsActive());
         verify(insertStmt).setString(6, session.getProblem().getType().toString());
         verify(insertStmt).setInt(7, session.getProblem().getId());
+        verify(insertStmt).setString(8, Mode.DO_ONE.name());
+        verify(insertPendingStepStmt).setInt(1, session.getId());
+        verify(insertPendingStepStmt).setInt(2, 21);
+        verify(insertPendingTaskStmt).setInt(1, session.getId());
+        verify(insertPendingTaskStmt).setInt(2, 13);
+        verify(insertPendingTaskStmt).setInt(3, 44);
     }
 
     @Test
@@ -130,14 +165,25 @@ public class SessionDAOTest {
         when(mockResultSet.getString(2)).thenReturn("token-100");
         when(mockResultSet.getDate(3)).thenReturn(Date.valueOf("2026-03-18"));
         when(mockResultSet.getBoolean(4)).thenReturn(true);
-        when(mockResultSet.getInt(6)).thenReturn(55);
+        when(mockResultSet.getInt(5)).thenReturn(1);
+        when(mockResultSet.getInt(6)).thenReturn(1);
+        when(mockResultSet.getInt(8)).thenReturn(55);
+        when(mockResultSet.getString(9)).thenReturn("TEACH_ONE");
+
+        PreparedStatement pendingStmt = mock(PreparedStatement.class);
+        ResultSet pendingRs = mock(ResultSet.class);
+        when(mockConnection.prepareStatement(startsWith("SELECT pt.TaskId"))).thenReturn(pendingStmt);
+        when(pendingStmt.executeQuery()).thenReturn(pendingRs);
+        when(pendingRs.next()).thenReturn(false);
 
         Problem problem = new LCSProblem(55, "ABC", "ABD");
+        CourseSvc mockCourseSvc = mock(CourseSvc.class);
         ProblemSvc mockProblemSvc = mock(ProblemSvc.class);
         when(mockProblemSvc.retrieve(55)).thenReturn(problem);
 
         try (MockedStatic<ServiceFactory> mockedServiceFactory = mockStatic(ServiceFactory.class)) {
             mockedServiceFactory.when(ServiceFactory::findProblemSvc).thenReturn(mockProblemSvc);
+            mockedServiceFactory.when(ServiceFactory::findCourseSvc).thenReturn(mockCourseSvc);
 
             Account account = new Account();
             account.setUserId("student@regis.edu");
@@ -150,6 +196,67 @@ public class SessionDAOTest {
             assertTrue(result.isIsActive());
             assertNotNull(result.getProblem());
             assertEquals(55, result.getProblem().getId());
+            assertEquals(Mode.TEACH_ONE, result.getMode());
+        }
+    }
+
+    @Test
+    public void testRetrieveLoadsPendingProgress() throws Exception {
+        PreparedStatement sessionStmt = mock(PreparedStatement.class);
+        ResultSet sessionRs = mock(ResultSet.class);
+        PreparedStatement pendingStmt = mock(PreparedStatement.class);
+        ResultSet pendingRs = mock(ResultSet.class);
+
+        when(mockConnection.prepareStatement(startsWith("SELECT SessionId"))).thenReturn(sessionStmt);
+        when(sessionStmt.executeQuery()).thenReturn(sessionRs);
+        when(sessionRs.next()).thenReturn(true);
+        when(sessionRs.getInt(1)).thenReturn(222);
+        when(sessionRs.getString(2)).thenReturn("token-222");
+        when(sessionRs.getDate(3)).thenReturn(Date.valueOf("2026-03-20"));
+        when(sessionRs.getBoolean(4)).thenReturn(true);
+        when(sessionRs.getInt(5)).thenReturn(1);
+        when(sessionRs.getInt(6)).thenReturn(1);
+        when(sessionRs.getInt(8)).thenReturn(55);
+        when(sessionRs.getString(9)).thenReturn("SEE_ONE");
+
+        when(mockConnection.prepareStatement(startsWith("SELECT pt.TaskId"))).thenReturn(pendingStmt);
+        when(pendingStmt.executeQuery()).thenReturn(pendingRs);
+        when(pendingRs.next()).thenReturn(true).thenReturn(false);
+        when(pendingRs.getInt(1)).thenReturn(13);
+        when(pendingRs.getInt(2)).thenReturn(400);
+        when(pendingRs.getInt(3)).thenReturn(21);
+        when(pendingRs.getBoolean(4)).thenReturn(true);
+        when(pendingRs.getBoolean(5)).thenReturn(false);
+        when(pendingRs.getInt(6)).thenReturn(3);
+
+        Problem problem = new LCSProblem(55, "ABC", "ABD");
+        Task restoredTask = new Task(13);
+        restoredTask.addStep(new Step(21, 0, StepSubType.INFO_MESSAGE));
+
+        CourseSvc mockCourseSvc = mock(CourseSvc.class);
+        ProblemSvc mockProblemSvc = mock(ProblemSvc.class);
+        when(mockProblemSvc.retrieve(55)).thenReturn(problem);
+        when(mockCourseSvc.retrieveTask(eq(1), eq(13), any(Connection.class))).thenReturn(restoredTask);
+
+        try (MockedStatic<ServiceFactory> mockedServiceFactory = mockStatic(ServiceFactory.class)) {
+            mockedServiceFactory.when(ServiceFactory::findProblemSvc).thenReturn(mockProblemSvc);
+            mockedServiceFactory.when(ServiceFactory::findCourseSvc).thenReturn(mockCourseSvc);
+
+            Account account = new Account();
+            account.setUserId("student@regis.edu");
+            Student student = new Student(account);
+
+            TutoringSession session = dao.retrieve(student);
+
+            assertEquals(Mode.SEE_ONE, session.getMode());
+            assertEquals(1, session.getTasks().size());
+            PendingTask pendingTask = session.getCurrentTask();
+            assertEquals(13, pendingTask.getTask().getId());
+            assertNotNull(pendingTask.getCurrentStep());
+            assertEquals(21, pendingTask.getCurrentStep().getStep().getId());
+            assertEquals(3, pendingTask.getCurrentStep().getCurrentHintIndex());
+            assertTrue(pendingTask.getCurrentStep().isNotifyTutor());
+            assertFalse(pendingTask.getCurrentStep().isCompleted());
         }
     }
 
@@ -191,14 +298,40 @@ public class SessionDAOTest {
     @Test
     public void testUpdateSuccess() throws Exception {
         TutoringSession session = buildSession(88, "student@regis.edu");
+        session.setMode(Mode.SEE_ONE);
+        PendingTask pendingTask = new PendingTask(new Task(7));
+        pendingTask.setCurrentStep(new PendingStep(new Step(11, 0, StepSubType.INFO_MESSAGE)));
+        session.addTask(pendingTask);
+
+        PreparedStatement clearPendingTaskStmt = mock(PreparedStatement.class);
+        PreparedStatement clearPendingStepStmt = mock(PreparedStatement.class);
+        PreparedStatement insertPendingStepStmt = mock(PreparedStatement.class);
+        PreparedStatement insertPendingTaskStmt = mock(PreparedStatement.class);
+        ResultSet keyRs = mock(ResultSet.class);
+
         when(mockConnection.prepareStatement(startsWith("UPDATE TutoringSession")))
                 .thenReturn(mockStatement);
+        when(mockConnection.prepareStatement(startsWith("DELETE FROM PendingTask")))
+            .thenReturn(clearPendingTaskStmt);
+        when(mockConnection.prepareStatement(startsWith("DELETE FROM PendingStep")))
+            .thenReturn(clearPendingStepStmt);
+        when(mockConnection.prepareStatement(startsWith("INSERT INTO PendingStep"), anyInt()))
+            .thenReturn(insertPendingStepStmt);
+        when(mockConnection.prepareStatement(startsWith("INSERT INTO PendingTask")))
+            .thenReturn(insertPendingTaskStmt);
         when(mockStatement.executeUpdate()).thenReturn(1);
+        when(insertPendingStepStmt.executeUpdate()).thenReturn(1);
+        when(insertPendingTaskStmt.executeUpdate()).thenReturn(1);
+        when(insertPendingStepStmt.getGeneratedKeys()).thenReturn(keyRs);
+        when(keyRs.next()).thenReturn(true);
+        when(keyRs.getInt(1)).thenReturn(15);
 
         assertDoesNotThrow(() -> dao.update(session));
 
         verify(mockConnection).setAutoCommit(false);
         verify(mockConnection).commit();
+        verify(mockStatement).setString(5, Mode.SEE_ONE.name());
+        verify(mockStatement).setInt(6, session.getId());
     }
 
     @Test
