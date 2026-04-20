c3f52b04a3cd2a6a8ca04cc72b140168371a80f0
Harrison Sherwin <harrison.sherwin@gmail.com>
2026-04-20 14:55:03 -0600

    Fix SvcFacadeTest race condition using CountDownLatch

--- src/test/java/edu/regis/dptu/test/SvcFacadeTest.java
+++ src/test/java/edu/regis/dptu/test/SvcFacadeTest.java
@@ -18,6 +18,7 @@ import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.jupiter.api.Test;
 
@@ -43,25 +44,31 @@ public class SvcFacadeTest {
 
     @Test
     public void tutorRequestParsesServerReply() throws Exception {
+        CountDownLatch serverReady = new CountDownLatch(1);
+
         Thread serverThread =
                 new Thread(
                         () -> {
                             try (ServerSocket serverSocket = new ServerSocket(53637);
-                                    Socket socket = serverSocket.accept();
-                                    BufferedReader in =
-                                            new BufferedReader(
-                                                    new InputStreamReader(
-                                                            socket.getInputStream()));
-                                    PrintWriter out =
-                                            new PrintWriter(socket.getOutputStream(), true)) {
-                                in.readLine();
-                                out.println("{\"status\":\"Hint\",\"data\":\"from-test-server\"}");
+                                    ) {
+                                serverReady.countDown();
+                                try (Socket socket = serverSocket.accept();
+                                        BufferedReader in =
+                                                new BufferedReader(
+                                                        new InputStreamReader(
+                                                                socket.getInputStream()));
+                                        PrintWriter out =
+                                                new PrintWriter(socket.getOutputStream(), true)) {
+                                    in.readLine();
+                                    out.println("{\"status\":\"Hint\",\"data\":\"from-test-server\"}");
+                                }
                             } catch (Exception ignored) {
                                 // Test asserts on client side response.
                             }
                         });
 
         serverThread.start();
+        serverReady.await();
         try {
             SvcFacade facade = SvcFacade.instance();
             ClientRequest request = new ClientRequest(ServerRequestType.REQUEST_HINT);
